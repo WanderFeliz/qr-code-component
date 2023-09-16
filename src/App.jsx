c3707e4d41f08614f0a1774546b332f7909203cb
Wander Xavier Paniagua Candelario <55289565+WanderFeliz-Octagono@users.noreply.github.com>
2023-09-16 16:29:25 -0400

    finished desktop view

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,99 @@
+import styled from "styled-components";
+import QrImage from "./assets/image-qr-code.png";
+
+const MainContainer = styled.div`
+  background-color: hsl(212, 45%, 89%);
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  /* - White: hsl(0, 0%, 100%)
+- Light gray: hsl(212, 45%, 89%)
+- Grayish blue: hsl(220, 15%, 55%)
+- Dark blue: hsl(218, 44%, 22%) */
+`;
+const Card = styled.div`
+  width: 270px;
+  height: 400px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: white;
+
+  padding: 15px;
+  border-radius: 20px;
+  box-shadow: 0px 0px 15px -8px black;
+
+  border: 1px;
+  border-color: hsl(220, 15%, 55%);
+`;
+const ImageContainer = styled.div`
+  width: 100%;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  border-radius: 10px;
+`;
+const Image = styled.img`
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+`;
+const CardBody = styled.div`
+  margin-top: 5px;
+  width: 90%;
+  text-align: center;
+`;
+const CardTitle = styled.h2`
+  font-size: 18px;
+  color: hsl(218, 44%, 22%);
+  font-weight: 700;
+`;
+const CardText = styled.p`
+
+  font-size: 15px;
+  color: hsl(220, 15%, 55%);
+  font-weight: 400;
+
+`;
 
 function App() {
-
   return (
     <>
-      <h1>HOLA</h1>
+      <MainContainer>
+        <Card>
+          <ImageContainer>
+            <Image src={QrImage} alt="qr-code-img"></Image>
+          </ImageContainer>
+          <CardBody>
+            <CardTitle>
+              Improve your front-end skills by building real projects
+            </CardTitle>
+            <CardText>
+              Scan the QR code to visit Frontend Mentor and take you coding
+              skills to the next level
+            </CardText>
+          </CardBody>
+        </Card>
+        {/* <div className="attribution">
+          Challenge by{" "}
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by{" "}
+          <a href="https://github.com/WanderFeliz">Wander Paniagua</a>.
+        </div> */}
+      </MainContainer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
